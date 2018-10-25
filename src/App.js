6017d243bb756cdede40177838b8c3a96c47e721
Pedro Mendonca <pedrom@sesame.local>
2018-10-25 21:46:34 +0100

    Basics on layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from'./imgs/logo.png'
+import jss from 'jss'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} alt="Logo" />
+          <h1>Anaesthesia W.G.R.</h1>
+          <i className="fas fa-chevron-down fa-2x"></i>
         </header>
+        <main>
+          <article>
+            <div className="bgContainer">
+              <img className="bg" src="/imgs/album_art1.png" alt="cover1" />
+            </div>
+            <p className="tiny">.01</p>
+            <img className="cover" src="/imgs/album_art1.png" alt="cover1" />
+            <footer>
+              <h3>Anaesthesia W.G.R.</h3>
+              <h2>Not About Kendrick Lamar</h2>
+            </footer>
+          </article>
+        </main>
       </div>
     );
   }
